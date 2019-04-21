cea48d098038628ebbd777236aac2f09525a6552
Jonathan Moallem <jdmoallem@gmail.com>
2019-04-21 19:47:35 +1000

    Binarised job and added it to the matrix

--- main.py
+++ main.py
@@ -17,11 +17,26 @@ df = pd.read_csv('banking_campaign.csv')
 binarised_marital = prep.binarise_marital(df['marital'])
 
 
+# Perform binarisation for 'job'
+binarised_job = prep.binarise_job(df['job'])
+
+
 # Create a combined data frame of pre-processed data for analysis
 processed_df = pd.DataFrame({
     'Married': binarised_marital['married'],
     'Single': binarised_marital['single'],
     'Divorced': binarised_marital['divorced'],
+    'Admin': binarised_job['admin'],
+    'Blue-Collar': binarised_job['blue-collar'],
+    'Entrepreneur': binarised_job['entrepreneur'],
+    'Housemaid': binarised_job['housemaid'],
+    'Management': binarised_job['management'],
+    'Retired': binarised_job['retired'],
+    'Self-Employed': binarised_job['self-employed'],
+    'Services': binarised_job['services'],
+    'Student': binarised_job['student'],
+    'Technician': binarised_job['technician'],
+    'Unemployed': binarised_job['unemployed'],
 })
 
 # Combine the original data to the processed data for analysis
--- scripts/pre_processing.py
+++ scripts/pre_processing.py
@@ -21,3 +21,49 @@ def binarise_marital(df):
             'single': binarised_single_list,
             'divorced': binarised_divorced_list}
 
+def binarise_job(df):
+    # Isolate the values from the data frame
+    df = df.values
+
+    # Binarise the categories
+    binarised = preprocessing.LabelBinarizer().fit_transform(df)
+
+    # Format the binarised items into 11 columns
+    binarised_admin_list = []
+    binarised_blue_collar_list = []
+    binarised_entrepreneur_list = []
+    binarised_housemaid_list = []
+    binarised_management_list = []
+    binarised_retired_list = []
+    binarised_self_employed_list = []
+    binarised_services_list = []
+    binarised_student_list = []
+    binarised_technician_list = []
+    binarised_unemployed_list = []
+    for i in range(len(binarised)):
+        item = binarised[i]
+        binarised_admin_list.append(item[0])
+        binarised_blue_collar_list.append(item[1])
+        binarised_entrepreneur_list.append(item[2])
+        binarised_housemaid_list.append(item[3])
+        binarised_management_list.append(item[4])
+        binarised_retired_list.append(item[5])
+        binarised_self_employed_list.append(item[6])
+        binarised_services_list.append(item[7])
+        binarised_student_list.append(item[8])
+        binarised_technician_list.append(item[9])
+        binarised_unemployed_list.append(item[10])
+
+    # Return the results as a dictionary
+    return {'admin': binarised_admin_list,
+            'blue-collar': binarised_blue_collar_list,
+            'entrepreneur': binarised_entrepreneur_list,
+            'housemaid': binarised_housemaid_list,
+            'management': binarised_management_list,
+            'retired': binarised_retired_list,
+            'self-employed': binarised_self_employed_list,
+            'services': binarised_services_list,
+            'student': binarised_student_list,
+            'technician': binarised_technician_list,
+            'unemployed': binarised_unemployed_list}
+
