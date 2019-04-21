cd65de701b788002cc2b0f38ee64f024b8d69109
Jonathan Moallem <jdmoallem@gmail.com>
2019-04-21 19:40:09 +1000

    Added function to display a correlation matrix with Seaborn

--- scripts/exploration.py
+++ scripts/exploration.py
@@ -3,6 +3,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from string import ascii_letters
+
+
+def correlation_matrix(df):
+
+    # Remove the row id from the data frame
+    df = df.drop('row id', 1)
+
+    # Set up the matrix plot
+    f, ax = plt.subplots(figsize=(10, 8))
+    corr = df.corr()
+    sns.heatmap(corr, mask=np.zeros_like(corr, dtype=np.bool),
+                cmap=sns.diverging_palette(220, 10, as_cmap=True),
+                square=True, ax=ax)
+
+    # Display the matrix
+    plt.show()
 
 
