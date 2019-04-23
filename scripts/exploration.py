1c2ab27624f819d2a8d70ff1dfa7059e4b22e975
Jonathan Moallem <jdmoallem@gmail.com>
2019-04-23 14:19:44 +1000

    Updated correlation matrix function

--- scripts/exploration.py
+++ scripts/exploration.py
@@ -3,17 +3,18 @@ import numpy as np
 import seaborn as sns
 
 
-def correlation_matrix(df):
-
+def correlation_matrix(df, title):
     # Remove the row id from the data frame
     df = df.drop('row id', 1)
 
-    # Set up the matrix plot
+    # Set up the matrix plot and display
     f, ax = plt.subplots(figsize=(20, 16))
     corr = df.corr()
     sns.heatmap(corr, mask=np.zeros_like(corr, dtype=np.bool),
                 cmap=sns.diverging_palette(220, 10, as_cmap=True),
-                square=True, ax=ax)
+                square=True, ax=ax).set_title(title)
+    plt.show()
+
 
     # Display the matrix
     plt.show()
